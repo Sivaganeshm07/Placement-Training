dd54064cbdbcf428ec60aaca4999f439ad0ad7e2
Sivaganesh <sivaganeshm07@gmail.com>
2025-02-13 16:04:54 +0530

    new file

--- Pattern.java
+++ Pattern.java
@@ -1,14 +1,15 @@
 class Pattern{
     public static void main(String args[]){
         int n = 5;
-        pattern1(n);
-        pattern2(n);
-        pattern3(n);
-        pattern4(n);
-        pattern5(n);
-        pattern6(n);
-        pattern7(n);
-        pattern8(n);
+        // pattern1(n);
+        // pattern2(n);
+        // pattern3(n);
+        // pattern4(n);
+        // pattern5(n);
+        // pattern6(n);
+        // pattern7(n);
+        // pattern8(n);
+        pattern9(n);
 
     }
     static void pattern1(int n){
@@ -127,5 +128,28 @@ class Pattern{
                 }
                 System.out.println();
             }
+    }
+    static void pattern9(int n){
+        //Upperhalf
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            if(i==n){
+                for(int k = 1;k<=2*n-1;k++){
+                System.out.print("*");
+                }
+            }
+            else {
+                System.out.print("*");
+                for (int j = 1; j <= (2 * i - 3); j++) {
+                    System.out.print("0");
+                }
+                if (i > 1) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
     }
 }
